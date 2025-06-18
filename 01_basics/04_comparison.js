049e825a55375afae53d72c715374313b3de4139
salim-web <154608933+salim-web@users.noreply.github.com>
2025-06-18 21:04:57 +0530

    Learned some of the inconsistencies in comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,21 @@
+console.log("2" > 1);
+console.log(2 > "1");
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+
+// strict comparison => checks datatype and also compares them
+console.log("2" === 2);
+
+// avoid comparisons of different datatypes 
+
+
+
+
+
+
