539156391b69a65730ff67c6a3b3786a1396d31b
salim-web <154608933+salim-web@users.noreply.github.com>
2025-06-18 11:10:18 +0530

    learned some datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict" // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs not browser
+
+let name = "Salim"
+let age = 19
+let isLoggedIn = false
+let state; // undefined
+let city = null // null
+
+// number => 2 to the power 53
+// bigint
+// string => "" ''
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => uniqueness
+
+// object
+
+// console.log(typeof null); // output => object
+// console.log(typeof undefined); // output => undefined
+
