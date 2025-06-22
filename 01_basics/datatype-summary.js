ba59636e9a8c8b3906e7f4ec80e48778e458e2b3
salim-web <154608933+salim-web@users.noreply.github.com>
2025-06-22 17:29:43 +0530

    Update datatype-summary.js

--- 01_basics/datatype-summary.js
+++ 01_basics/datatype-summary.js
@@ -1,6 +1,6 @@
-//  Primitive
+//  Primitive(copy/immutable)
 
-//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+//  7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
 
 const score = 100
 const scoreValue = 100.3
@@ -12,13 +12,13 @@ let userEmail;
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+// console.log(id === anotherId);
 
 // const bigNumber = 3456543576654356754n
 
 
 
-// Reference (Non primitive)
+// Reference/immutable (Non primitive)
 
 // Array, Objects, Functions
 
@@ -32,6 +32,33 @@ const myFunction = function(){
     console.log("Hello world");
 }
 
-console.log(typeof heros);
+// console.log(typeof heros);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (Primitive), Heap (Non-Primitive)
+
+let myYoutubeName = "saliiim"
+
+let anotherName = myYoutubeName
+anotherName = "salim"
+
+console.log(myYoutubeName);
+console.log(anotherName);
+
+let userOne = {
+    user : "user@google.com",
+    upi : "user@ybl"
+}
+
+let userTwo = userOne
+userTwo.email = "salim@google.com"
+
+console.log(userOne);
+console.log(userTwo);
+
