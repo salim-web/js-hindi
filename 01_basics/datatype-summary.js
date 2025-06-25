dc42722b35538db299222d3179c191b6351ca4b5
salim-web <154608933+salim-web@users.noreply.github.com>
2025-06-25 11:12:52 +0530

    Update datatype-summary.js

--- 01_basics/datatype-summary.js
+++ 01_basics/datatype-summary.js
@@ -12,9 +12,9 @@ let userEmail;
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-// console.log(id === anotherId);
+console.log(id === anotherId);
 
-// const bigNumber = 3456543576654356754n
+const bigNumber = 3456543576654356754n
 
 
 
@@ -32,7 +32,7 @@ const myFunction = function(){
     console.log("Hello world");
 }
 
-// console.log(typeof heros);
+console.log(typeof heros);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
 
