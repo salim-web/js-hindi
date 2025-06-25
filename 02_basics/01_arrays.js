4838e7f3d8e90c1ea0c5f5c96fe14b9c07eb20f7
salim-web <154608933+salim-web@users.noreply.github.com>
2025-06-25 11:12:55 +0530

    Update 01_arrays.js

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,26 +1,26 @@
 const myArr = [0, 1, 2, 3, 4, 5, true, null, 'salim']
-// console.log(myArr[7]);
+console.log(myArr[7]);
 
 const myArr2 = new Array(0, 1, 2, 3, 4)
 
-// myArr.push(6)
-// console.log(myArr);
+myArr.push(6)
+console.log(myArr);
 
-// myArr.pop()
-// console.log(myArr);
+myArr.pop()
+console.log(myArr);
 
-// myArr2.unshift(0) // adds at the starting of the array
-// console.log(myArr2);
-// myArr2.shift() // removes from the first position
-// console.log(myArr2);
+myArr2.unshift(0) // adds at the starting of the array
+console.log(myArr2);
+myArr2.shift() // removes from the first position
+console.log(myArr2);
 
-// console.log(myArr2.includes(9)); // boolean
-// console.log(myArr2.indexOf(3)); // 2
-// console.log(myArr2.indexOf(7)); //if not in range ouputs -1 
+console.log(myArr2.includes(9)); // boolean
+console.log(myArr2.indexOf(3)); // 2
+console.log(myArr2.indexOf(7)); //if not in range ouputs -1 
 
-// const newArr = myArr2.join() // datatype string
-// console.log(myArr2);
-// console.log(newArr);
+const newArr = myArr2.join() // datatype string
+console.log(myArr2);
+console.log(newArr);
 
 // slice, splice
 
