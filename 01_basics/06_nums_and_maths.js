9972cc9731469fedcf7670877e528b188e7d8cc4
salim-web <154608933+salim-web@users.noreply.github.com>
2025-06-25 11:12:41 +0530

    Update 06_nums_and_maths.js

--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -1,30 +1,30 @@
 const balance = new Number(400)
 
-// console.log(balance.toString().length);
-// console.log(balance.toFixed(2));
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
 
 const otherNumber = 23.8902
-// console.log( otherNumber.toPrecision(2)); // returns a string value after rounding off
+console.log( otherNumber.toPrecision(2)); // returns a string value after rounding off
 
 hundreds = 10000000
-// console.log(hundreds.toLocaleString("en-IN"));
+console.log(hundreds.toLocaleString("en-IN"));
 
 
 // ++++++++++++++++++++++++ Maths ++++++++++++++++++++++++++++++++++
 
 
-// console.log(Math.PI);
-// console.log(Math.abs(-300));
-// console.log(Math.round(345.89));
-// console.log(Math.ceil(4.2)); // 5
-// console.log(Math.floor(4.9)); //4
-// console.log(Math.min(2,4,5,1));
-// console.log(Math.max(2,4,5,1));
+console.log(Math.PI);
+console.log(Math.abs(-300));
+console.log(Math.round(345.89));
+console.log(Math.ceil(4.2)); // 5
+console.log(Math.floor(4.9)); //4
+console.log(Math.min(2,4,5,1));
+console.log(Math.max(2,4,5,1));
 
-// console.log(Math.random()); // 0-1
-// console.log(Math.random() * 10); // 1-10
-// console.log((Math.random() * 10) + 1); // wont give 0
-// console.log(Math.floor(Math.random() * 10) + 1); 
+console.log(Math.random()); // 0-1
+console.log(Math.random() * 10); // 1-10
+console.log((Math.random() * 10) + 1); // 1 is added so that the min output no => 1
+console.log(Math.floor(Math.random() * 10) + 1); 
 
 const min = 0
 const max = 6
