58335062598595733728b1662dba4cab0077c1db
salim-web <154608933+salim-web@users.noreply.github.com>
2025-06-25 10:17:07 +0530

    Update 05_strings.js

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -52,6 +52,7 @@ charCode = "💖"
 // console.log(`The given sentence ${exampleString.includes("brown") ? "does" : "doesn't"} include brown`,);
 // console.log(exampleString.includes("fence") ? "does" :"doesn't");
 
+// console.log(exampleString.length);
 
 
  
