a12a50c9a6361d6e6b7b449266cdb819d9ba1e1b
salim-web <154608933+salim-web@users.noreply.github.com>
2025-06-26 11:50:02 +0530

    Update 03_conversionOperation.js

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,18 +1,18 @@
 let score = "33"
 let scoreTwo = "33abc"
 
-// const {score} = req.body // user input so we don't know the datatype
+const {score} = req.body // user input so we don't know the datatype
 
-// console.log(typeof score)
-// console.log(typeof(score));
+console.log(typeof score)
+console.log(typeof(score));
 
 let valueInNumber = Number(score) // converts string to number(should be written in upper case)
 
-// console.log(typeof(valueInNumber));
+console.log(typeof(valueInNumber));
 
 let valueInNumberTwo = Number(scoreTwo)
-// console.log(typeof(valueInNumberTwo)); // datatype => number
-// console.log(valueInNumberTwo);  // value => NaN 
+console.log(typeof(valueInNumberTwo)); // datatype => number
+console.log(valueInNumberTwo);  // value => NaN 
 
 /* "33" => 33
 "33abc" => NaN (type of NaN is number)
@@ -21,7 +21,7 @@ Type of null is object */
 
 let isLoggenIn = 1
 let booleanIsLoggenIn =  Boolean(isLoggenIn)
-// console.log(booleanIsLoggenIn);
+console.log(booleanIsLoggenIn);
 
 /* 1 => true; 0 = false
 "" => false
@@ -29,20 +29,20 @@ let booleanIsLoggenIn =  Boolean(isLoggenIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-// console.log(stringNumber);
-// console.log(typeof stringNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
 
 
 // ************************************Operations*************************************************
 
 let value = 3
 let negValue = -value
-// console.log(negValue);
+console.log(negValue);
 
 let str1 = "hello"
 let str2 = " salim"
 let str3 = str1 + str2 // str1 - str2 => NaN
-// console.log(str3);
+console.log(str3);
 
 console.log("1" + 2); // => 12
 console.log(1 + "2"); // => 12
