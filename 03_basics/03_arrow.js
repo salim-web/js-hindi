f101eab670964f16554913db0e8021ed55adbcf0
salim-web <154608933+salim-web@users.noreply.github.com>
2025-07-01 21:15:42 +0530

    Update 03_arrow.js

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -8,49 +8,47 @@ const user = {
     }
 }
 
-user.welcomeMessage()
+user.welcomeMessage() // returns an object
 user.userName = 'sam'
-user.welcomeMessage()
-console.log(this.userName); // returns empty obj
-// still console log is undefined and this function returns values
+user.welcomeMessage() // returns an object
+console.log(this.userName); // returns undefined
+// this.userName is undefined and this function returns values
 
 
 function cyber() {
     let userName = 'salim'
     console.log(this.userName);
 }
-// still console log is undefined and this function returns values
+// still this.userName is undefined and this function returns a value
 
 
 const cyber = function() {
     let userName = 'salim'
-    console.log(this.userName);
+    console.log(this.userName); // console.log(this)
 }
-// still console log is undefined and this function returns values
+cyber()
+// // still console log is undefined  and this function returns values
 
 
 const cyber = () => {
     let userName = 'salim'
-    console.log(this.userName); 
-    
-}
-// still console log is undefined but this function returns an empty set now
+    console.log(this); 
+}   
+// this function returns an empty set now and this.username is udefined
 
-// cyber()
+
 
 const addTwo = (num1, num2) => {
     return num1 + num2
 }
-//basic arrow function
+// //basic arrow function
 
-const addTwo = (num1, num2) => num1 + num2
-
-const addTwo = (num1, num2) => ( num1 + num2 )
+const addTwo = (num1, num2) => num1 + num2  // or const addTwo = (num1, num2) => ( num1 + num2 )
 
 console.log(addTwo(2,4));
+// //implicit return
 
 const obj = (num1, num2) => ({userName : 'salmis'})
 
 console.log(obj());
 
-//implicit return
