eda61fed61cac0b2dd54a0ad44ae722b19de891f
salim-web <154608933+salim-web@users.noreply.github.com>
2025-07-02 20:57:58 +0530

    Create switch.js

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,31 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 4
+
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("February");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+
+
+    default:
+        console.log("Try giving valid syntazx");
+        break;
+}
+// if break is not given after each cases then it will run every case after the matched case except default
