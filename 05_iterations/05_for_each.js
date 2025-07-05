f16096649c491fbffe89f49575a9e392247e99c5
salim-web <154608933+salim-web@users.noreply.github.com>
2025-07-05 12:15:35 +0530

    Update 05_for_each.js

--- 05_iterations/05_for_each.js
+++ 05_iterations/05_for_each.js
@@ -1,44 +1,41 @@
-const coding = ['js', 'py', 'rb', 'cpp', 'java']
+const coding = ["js", "py", "rb", "cpp", "java"];
 
-coding.forEach( function ( item ) {
-    // console.log(item);
-    
-})
+coding.forEach(function (item) {
+  // console.log(item);
+});
 
-coding.forEach( ( item ) => {
-    // console.log(item);
-    
-})
+coding.forEach((item) => {
+  // console.log(item);
+});
 
 function print(params) {
-    console.log(params);
+  console.log(params);
 }
 
 // coding.forEach( print )  // only the reference of the function shall be given not to be executed i.e,  not paranthesis
 
-coding.forEach( (item, index, arr) => {
-    // console.log(item,index,arr);
-    // print(`The values in the array are: ${item}`)
-} )
+coding.forEach((item, index, arr) => {
+  // console.log(item,index,arr);
+  // print(`The values in the array are: ${item}`)
+});
 
 // for each fetches values, index an the whole array in this order
 
 const myCoding = [
-    {
-        languageName : "Javascript",
-        languageFileName : "js"
-    },
-    {
-        languageName : "Java",
-        languageFileName : "java"
-    },
-    {
-        languageName : "python",
-        languageFileName : "py"
-    }
-]
+  {
+    languageName: "Javascript",
+    languageFileName: "js",
+  },
+  {
+    languageName: "Java",
+    languageFileName: "java",
+  },
+  {
+    languageName: "python",
+    languageFileName: "py",
+  },
+];
 
-myCoding.forEach( (item) => {
-    // console.log(item.languageName, ' file name is', item.languageFileName);
-    
+myCoding.forEach((item) => {
+  // console.log(item.languageName, ' file name is', item.languageFileName);
 });
