9a3d98d93b98299365d69f5e3214addd889fbf96
salim-web <154608933+salim-web@users.noreply.github.com>
2025-07-05 12:15:27 +0530

    Update 02_while_dowhile.js

--- 05_iterations/02_while_dowhile.js
+++ 05_iterations/02_while_dowhile.js
@@ -1,25 +1,22 @@
-let index = 1
-while (index<=100) {
-    // console.log(`Value of index: ${index}`);
-    index = index + 2
-    // index++ //index +1
-    // index += index // to the power
+let index = 1;
+while (index <= 100) {
+  // console.log(`Value of index: ${index}`);
+  index = index + 2;
+  // index++ //index +1
+  // index += index // to the power
 }
 
-myArray = ["Rimuru", "Gobta", "Ranga", "Gobzo"]
+myArray = ["Rimuru", "Gobta", "Ranga", "Gobzo"];
 
-arr = 0
+arr = 0;
 while (arr < myArray.length) {
-    // console.log(`Value is ${myArray[arr]}`);
-    arr++;
-    
+  // console.log(`Value is ${myArray[arr]}`);
+  arr++;
 }
 
-let score = 1
+let score = 1;
 
 do {
-    
-    console.log(`Score is ${score}`);
-    score++
-    
+  console.log(`Score is ${score}`);
+  score++;
 } while (score <= 10);
