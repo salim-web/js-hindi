b8f0ad6052d334715768397335b8e5deb01bf1ba
salim-web <154608933+salim-web@users.noreply.github.com>
2025-07-05 12:15:23 +0530

    Update 01_for.js

--- 05_iterations/01_for.js
+++ 05_iterations/01_for.js
@@ -1,47 +1,47 @@
 // for
 
 for (let i = 0; i < 10; i++) {
-    const element = i;
-    // console.log(element);
+  const element = i;
+  // console.log(element);
 }
 
 for (let i = 0; i <= 10; i++) {
-    // console.log(`Outer Loop value: ${i}`);
-    for (let j = 0; j <= 10; j++) {
-        // console.log(`Inner Loop value: ${j} and inner loop ${i}`);
-    }
+  // console.log(`Outer Loop value: ${i}`);
+  for (let j = 0; j <= 10; j++) {
+    // console.log(`Inner Loop value: ${j} and inner loop ${i}`);
+  }
 }
 
 for (let i = 1; i <= 10; i++) {
-    // console.log(`\nTable of ${i}\n`)
-    for(let j = 1; j <= 10; j++) {
-        // console.log(`${i} x ${j} = ${i*j}`);
-    }
+  // console.log(`\nTable of ${i}\n`)
+  for (let j = 1; j <= 10; j++) {
+    // console.log(`${i} x ${j} = ${i*j}`);
+  }
 }
 
-myArray = ["Ron", "Toto", "Eliot", "Winter"]
+myArray = ["Ron", "Toto", "Eliot", "Winter"];
 // console.log(`Length of the Array: ${myArray.length}`);
 
 for (let index = 0; index < myArray.length; index++) {
-    const element = myArray[index];
-    // console.log(element);
+  const element = myArray[index];
+  // console.log(element);
 }
 
 //break and continue
 
 for (let index = 1; index < 20; index++) {
-    // console.log(`Value of index is: ${index}`);
-    if (index == 5) {
-        // console.log("5 detected");
-        break;
-    }    
+  // console.log(`Value of index is: ${index}`);
+  if (index == 5) {
+    // console.log("5 detected");
+    break;
+  }
 }
 for (let index = 1; index < 20; index++) {
-    if (index == 5) {
-        console.log("5 detected");
-        continue
-    }    
-    console.log(`Value of index is: ${index}`);
+  if (index == 5) {
+    console.log("5 detected");
+    continue;
+  }
+  console.log(`Value of index is: ${index}`);
 }
 
 // here 5 detected will get printed but value of index: 5 will not get printed and the program will continue
