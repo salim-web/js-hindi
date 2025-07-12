646b7e0d7ba0527d3e5621ac50b928cd6f068464
salim-web <154608933+salim-web@users.noreply.github.com>
2025-07-12 19:48:26 +0530

    Update 04_comparison.js

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -1,17 +1,26 @@
-// console.log("2" > 1); // output => true
-// console.log(2 > "1"); // output => true
+console.log("2" > 1); // output => true
+console.log(2 > "1"); // output => true
 
-// console.log(null > 0); // output => false
-// console.log(null == 0); // output => false
-// console.log(null >= 0); // output => true
+console.log(null > 0); // output => false
+console.log(null == 0); // output => false
+console.log(null >= 0); // output => true
 
-// console.log(undefined > 0); // output => false
-// console.log(undefined == 0); // output => false
-// console.log(undefined >= 0); // output => false
+console.log(undefined > 0); // output => false
+console.log(undefined == 0); // output => false
+console.log(undefined >= 0); // output => false
 
-// // strict comparison => checks datatype and also compares them
-// console.log("2" === 2); // output => false
+// strict comparison => checks datatype and also compares them
+console.log("2" === 2); // output => false
 
-// // avoid comparisons of different datatypes 
+let x = false;
+let y = "0";
+let z = 0;
 
+console.log(x == y); // true
+console.log(x == z); // true
+console.log(y == x); //true 
+
+
+// avoid comparisons of different datatypes 
+// coersion => when we use == it converts the datatypes, in this case it converts the string into number then number into boolean
  
