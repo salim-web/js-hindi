56b03e560fdda472607e64cf8df8f0e5b35f11c3
salim-web <154608933+salim-web@users.noreply.github.com>
2025-07-12 19:44:04 +0530

    Update 04_comparison.js

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -1,15 +1,17 @@
-console.log("2" > 1); // output => true
-console.log(2 > "1"); // output => true
+// console.log("2" > 1); // output => true
+// console.log(2 > "1"); // output => true
 
-console.log(null > 0); // output => false
-console.log(null == 0); // output => false
-console.log(null >= 0); // output => true
+// console.log(null > 0); // output => false
+// console.log(null == 0); // output => false
+// console.log(null >= 0); // output => true
 
-console.log(undefined > 0); // output => false
-console.log(undefined == 0); // output => false
-console.log(undefined >= 0); // output => false
+// console.log(undefined > 0); // output => false
+// console.log(undefined == 0); // output => false
+// console.log(undefined >= 0); // output => false
 
-// strict comparison => checks datatype and also compares them
-console.log("2" === 2); // output => false
+// // strict comparison => checks datatype and also compares them
+// console.log("2" === 2); // output => false
 
-// avoid comparisons of different datatypes 
+// // avoid comparisons of different datatypes 
+
+ 
